8c7788eb4acd6a5889936766919ee7fb8d270e2b
lucasgianine <lucas.esilva@sptech.school>
2023-06-02 00:12:26 -0300

    Update: Plotando o gráfico de voto dos jogadores

--- Site/src/controllers/votoController.js
+++ Site/src/controllers/votoController.js
@@ -2,13 +2,31 @@ var votoModel = require("../models/votoModel");
 
 var sessoes = [];
 
-function testar(req, res) {
-    console.log("ENTRAMOS NA usuarioController");
-    res.json("ESTAMOS FUNCIONANDO!");
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var insertId = req.body.insertIdServer;
+    var jogadorVotado = req.body.jogadorVotadoServer;
+
+    // Passe os valores como parâmetro e vá para o arquivo votoModel.js
+    votoModel.cadastrar(insertId, jogadorVotado)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
 }
 
-function listar(req, res) {
-    votoModel.listar()
+function votos(req, res) {
+    votoModel.votos()
         .then(function (resultado) {
             if (resultado.length > 0) {
                 res.status(200).json(resultado);
@@ -24,65 +42,7 @@ function listar(req, res) {
         );
 }
 
-function entrar(req, res) {
-    var usuario = req.body.usuarioServer;
-    var senha = req.body.senhaServer;
-
-    if (usuario == undefined) {
-        res.status(400).send("Seu usuario está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
-        
-        votoModel.entrar(usuario, senha)
-            .then(
-                function (resultado) {
-                    console.log(`\nResultados encontrados: ${resultado.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
-
-                    if (resultado.length == 1) {
-                        console.log(resultado);
-                        res.json(resultado[0]);
-                    } else if (resultado.length == 0) {
-                        res.status(403).send("Usuário e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
-
-}
-
-function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var insertId = req.body.insertIdServer;
-    var jogadorVotado = req.body.jogadorVotadoServer;
-        
-        // Passe os valores como parâmetro e vá para o arquivo votoModel.js
-        votoModel.cadastrar(insertId, jogadorVotado)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
-
 module.exports = {
     cadastrar,
+    votos
 }
--- Site/src/routes/voto.js
+++ Site/src/routes/voto.js
@@ -8,4 +8,9 @@ router.post("/cadastrar", function (req, res) {
     votoController.cadastrar(req, res);
 })
 
+// Para buscar no banco de dados a quantidade de votos
+router.get("/votos", function (req, res) {
+    votoController.votos(req, res);
+});
+
 module.exports = router;
